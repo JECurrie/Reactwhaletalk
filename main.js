f69d568a88165cb492f9cd960fcf3ed4634e5c4a
Joan Currie <jecurrie8@gmail.com>
2018-08-09 03:26:28 +0000

    Whale Talk

---
+++ main.js
@@ -0,0 +1,26 @@
+//Whale Talk:
+//No consonants and the us and es are doubled.
+
+const input = "Cheese is nice and I like it sometimes";
+//const input = 'turpentine and turtles'
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+     resultArray.push(input[i]);
+    }
+  }
+
+  if (input[i] === 'e') {
+     resultArray.push(input[i]);
+  }
+  if (input[i] === 'u') {
+     resultArray.push(input[i]);
+  }
+}
+
+console.log(resultArray);
